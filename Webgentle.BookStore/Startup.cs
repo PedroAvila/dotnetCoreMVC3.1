41e956b04f93c814d46ebd8e7111ed1201fd0eaa
PedroAvila <minimainor_austin@hotmail.com>
2021-05-11 16:29:51 -0500

    Middleware in Asp.net Core | app.Use(), app.Next(), app.Map() | Http Pipeline

--- Webgentle.BookStore/Startup.cs
+++ Webgentle.BookStore/Startup.cs
@@ -26,6 +26,26 @@ namespace Webgentle.BookStore
                 app.UseDeveloperExceptionPage();
             }
 
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("Hello from my first middleware");
+            //    await next();
+            //    await context.Response.WriteAsync("Hello from my first middleware response");
+            //});
+
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("Hello from my second middleware");
+            //    await next();
+            //    await context.Response.WriteAsync("Hello from my second middleware response");
+            //});
+
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("Hello from my third middleware");
+            //    await next(); 
+            //});
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
@@ -33,6 +53,16 @@ namespace Webgentle.BookStore
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
+                });
+            });
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/nitish", async context =>
+                {
+                    await context.Response.WriteAsync("Hello Nitish!");
                 });
             });
         }
